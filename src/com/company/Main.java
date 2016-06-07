55132bd9d0b4a03270aab01cf0f22fe4e712d4a5
Michael Jeffress <michaeljeffress@Michaels-MacBook-Pro.local>
2016-06-07 16:58:40 -0700

    Added code from Part 1 into new ItelliJ project

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int three = 3;
+
+        String string1 = "Peter";
+        String string2 = "Paul";
+        String string3 = "Mary";
+    }
+}
